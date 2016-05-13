c9c617750d4104a4a5ce0ed478dc4e3e1c808bc8
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-05-13 12:45:40 +0900

    add multiple cxense command

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var config_cxense = require('./config/config_cxense.conf');
 var https = require('https');
 
 var count = 0;
+var api_num=3;
 var username = 'cxense-team@dac.co.jp';
 var apiKey = config_cxense.api_key;
 var date = new Date().toISOString();
@@ -18,28 +19,29 @@ exports.handler = (event, context, callback) => {
     var userid = event.userid;
     console.log("userid is " + userid);
 
-
+//    cxense_traffic(event, context);
     retrieve_profile_user(event, context, userid);
     retrieve_traffic_keyword(event, context, userid);
-    retrieve_traffic_event(event, context, userid);
+   retrieve_traffic_event(event, context, userid);
 
 };
+/*
+function cxense_traffic(event, context){
 
-function retrieve_profile_user(event, context, userid){
+	console.log("---cxense_traffic is called---");
 
-	console.log("--retrieve_profile_user start--");
-
-    var str_concept = "dac";
+	var str_concept = "dac";
 	var filter_obj = { "type":"keyword","group":"concept","item":str_concept };
 	var filters_array = new Array(filter_obj);
 	var fields_array = ["events","urls","activeTime"];
 
 	var postData = JSON.stringify({
 	  	siteId: '1128275557251903601',
-	  	start: -8640000,
+	  	start: -86400,
 	  	fields: fields_array,
 	  	filters: filters_array
 	});
+	console.log(postData);
 
 	var option = {
 		hostname: "api.cxense.com",
@@ -61,8 +63,67 @@ function retrieve_profile_user(event, context, userid){
 			console.log(chunk);
 		});
 		res.on('end', function(){
+			console.log("request ended successfully in cxense_traffic");
+			count++;
+			if(count==api_num){
+    			context.succeed(event);
+	    	}
+		});
+	});
+
+	req.on("error", function(error){
+		console.log("request fail fail fail in cxense_traffic");
+		context.succeed(event);
+	});
+
+	req.write(postData);
+	req.end();
+
+}
+*/
+/*
+python cx.py /profile/user '{"id":"64f3fb36-7a9b-4156-a584-bd9a86d0ddfa", "type":"dac"}'
+
+
+*/
+
+
+function retrieve_profile_user(event, context, userid){
+
+	console.log("--retrieve_profile_user start--");
+
+	var postData = JSON.stringify({
+		id:userid,
+	  	type:"dac"
+	});
+	console.log(postData);
+
+	var option = {
+		hostname: "api.cxense.com",
+		path: "/profile/user",
+		method: "POST",
+		port: 443,
+		headers: {
+			'X-cXense-Authentication': 'username=' + username + ' date=' + date + ' hmac-sha256-hex=' + hmac,
+	  		'Content-Type': 'application/json;charset=utf-8'
+		}
+	};
+
+	var req = https.request(option, function(res){
+		console.log(res.statusCode);
+		console.log(res.headers);
+		res.setEncoding('utf8');
+		res.on('data', function(chunk){
+			console.log("----data chunk of retrieve_profile_user---");
+			console.log(chunk);
+		});
+		res.on('end', function(){
 			console.log("request ended successfully in retrieve_profile_user");
     		context.succeed(event);
+			count++;
+			if(count==api_num){
+				context.succeed(event);
+	    	}
 		});
 	});
 
@@ -71,6 +132,7 @@ function retrieve_profile_user(event, context, userid){
 		context.succeed(event);
 	});
 
+	req.write(postData);
 	req.end();
 
 }
@@ -78,9 +140,106 @@ function retrieve_profile_user(event, context, userid){
 function retrieve_traffic_keyword(event, context, userid){
 
 
+	console.log("--retrieve_traffic_keyword start--");
+
+	var postData = JSON.stringify({
+		siteId:'1128275557251903601',
+		filters:[{type:"user",group:"dac", item:userid}]
+	});
+
+	console.log(postData);
+
+	var option = {
+		hostname: "api.cxense.com",
+		path: "/traffic/keyword",
+		method: "POST",
+		port: 443,
+		headers: {
+			'X-cXense-Authentication': 'username=' + username + ' date=' + date + ' hmac-sha256-hex=' + hmac,
+	  		'Content-Type': 'application/json;charset=utf-8'
+		}
+	};
+
+	var req = https.request(option, function(res){
+		console.log(res.statusCode);
+		console.log(res.headers);
+		res.setEncoding('utf8');
+		res.on('data', function(chunk){
+			console.log("----data chunk of retrieve_traffic_keyword----");
+			console.log(chunk);
+		});
+		res.on('end', function(){
+			console.log("request ended successfully in retrieve_traffic_keyword");
+			if(count==api_num){
+    			context.succeed(event);
+    		}
+		});
+	});
+
+	req.on("error", function(error){
+		console.log("request fail fail fail in retrieve_traffic_keyword ");
+		count++;
+		if(count==api_num){
+			context.succeed(event);
+    	}
+	});
+
+	req.write(postData);
+	req.end();
+
 }
 
 
 function retrieve_traffic_event(event, context, userid){
+
+
+	console.log("--retrieve_traffic_keyword start--");
+
+	var postData = JSON.stringify({
+		siteId:'1128275557251903601',
+		filters:[{type:"user",group:"dac", item:userid}]
+	});
+
+	console.log(postData);
+
+	var option = {
+		hostname: "api.cxense.com",
+		path: "/traffic/event",
+		method: "POST",
+		port: 443,
+		headers: {
+			'X-cXense-Authentication': 'username=' + username + ' date=' + date + ' hmac-sha256-hex=' + hmac,
+	  		'Content-Type': 'application/json;charset=utf-8'
+		}
+	};
+
+	var req = https.request(option, function(res){
+		console.log(res.statusCode);
+		console.log(res.headers);
+		res.setEncoding('utf8');
+		res.on('data', function(chunk){
+			console.log("----data chunk of retrieve_traffic_event----");
+			console.log(chunk);
+		});
+		res.on('end', function(){
+			console.log("request ended successfully in retrieve_traffic_event");
+			if(count==api_num){
+    			context.succeed(event);
+    		}
+		});
+	});
+
+	req.on("error", function(error){
+		console.log("request fail fail fail in retrieve_traffic_event ");
+		count++;
+		if(count==api_num){
+			context.succeed(event);
+    	}
+	});
+
+	req.write(postData);
+	req.end();
+
+
 
 }
