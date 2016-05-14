ffe5bd6aad96d865664de7b6b6dd2870c569d84b
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-05-14 13:19:16 +0900

    add cxense command and retrieve data and set it in the dynamodb

--- index.js
+++ index.js
@@ -11,6 +11,23 @@ var apiKey = config_cxense.api_key;
 var date = new Date().toISOString();
 var hmac = crypto.createHmac('sha256', apiKey).update(date).digest('hex');
 
+var user_primary_key = null
+
+
+var config = require('./config/config_aws.conf');
+//var config = require('./config/config_local.conf');
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: config.dynamo_region,
+  endpoint: config.dynamo_url
+});
+
+
+ var aws_client = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient();
+
+
 
 exports.handler = (event, context, callback) => {
     console.log('----------cxense userinfo update is called---------');
@@ -18,69 +35,84 @@ exports.handler = (event, context, callback) => {
 
     var userid = event.userid;
     console.log("userid is " + userid);
+    user_primary_key = retrieve_primary_key(event, context, userid);
 
 //    cxense_traffic(event, context);
     retrieve_profile_user(event, context, userid);
     retrieve_traffic_keyword(event, context, userid);
-   retrieve_traffic_event(event, context, userid);
-
+	retrieve_traffic_event(event, context, userid);
 };
-/*
-function cxense_traffic(event, context){
 
-	console.log("---cxense_traffic is called---");
 
-	var str_concept = "dac";
-	var filter_obj = { "type":"keyword","group":"concept","item":str_concept };
-	var filters_array = new Array(filter_obj);
-	var fields_array = ["events","urls","activeTime"];
+function retrieve_primary_key(event, context, userid){
 
-	var postData = JSON.stringify({
-	  	siteId: '1128275557251903601',
-	  	start: -86400,
-	  	fields: fields_array,
-	  	filters: filters_array
-	});
-	console.log(postData);
-
-	var option = {
-		hostname: "api.cxense.com",
-		path: "/traffic",
-		method: "POST",
-		port: 443,
-		headers: {
-			'X-cXense-Authentication': 'username=' + username + ' date=' + date + ' hmac-sha256-hex=' + hmac,
-	  		'Content-Type': 'application/json;charset=utf-8'
-		}
+	var params = {
+	    TableName: "User",
+	    IndexName: "TuuidIndex",
+	    KeyConditionExpression: "tuuid = :tuuid_value",
+	    ExpressionAttributeValues: {
+	        ":tuuid_value": userid
+	    },
+	    ProjectionExpression: "first_name, last_name, email"
 	};
-
-	var req = https.request(option, function(res){
-		console.log(res.statusCode);
-		console.log(res.headers);
-		res.setEncoding('utf8');
-		res.on('data', function(chunk){
-			console.log("data chunk");
-			console.log(chunk);
-		});
-		res.on('end', function(){
-			console.log("request ended successfully in cxense_traffic");
-			count++;
-			if(count==api_num){
-    			context.succeed(event);
-	    	}
-		});
+	docClient.query(params, function(err, data) {
+	    if (err){
+	        console.log(JSON.stringify(err, null, 2));
+	    }else{
+	        console.log(JSON.stringify(data, null, 2));
+	        if(!data.Items[0]){
+	        	console.log("no user with this id" + data);
+	        	context.succeed(event);
+	        	return;
+	        }
+	        var email_address = data.Items[0].email;
+	        if(email_address){
+	            console.log("primary key is " + email_address)
+	            //update_name(email_address, "aaa");
+	            user_primary_key = email_address;
+	        }else{
+	        	console.log("primary key cannot be found");
+	        	context.succeed(event);
+	        	return;
+	        }
+	    }
 	});
-
-	req.on("error", function(error){
-		console.log("request fail fail fail in cxense_traffic");
-		context.succeed(event);
-	});
-
-	req.write(postData);
-	req.end();
-
 }
-*/
+
+function set_userdata(event, context, in_email_address, in_attr_name, in_attr_value){
+
+	console.log("-------------set_userdata is called: key" + in_attr_name + "--------------")
+    var params = {
+        TableName: "User",
+        Key: {
+            "email":in_email_address
+        },
+        UpdateExpression: "SET #attribute_name = :attribute_value",
+        ExpressionAttributeValues: { 
+            ":attribute_value": in_attr_value
+        },
+        ExpressionAttributeNames: {
+            "#attribute_name": in_attr_name
+        },
+        ReturnValues: "ALL_NEW"
+    };
+    docClient.update(params, function(err, data) {
+        if (err){
+            console.log(JSON.stringify(err, null, 2));
+        }
+        else{
+            console.log(JSON.stringify(data, null, 2));
+            console.log("storing data succeed. key is " + in_attr_name);
+        }
+		count++;
+		if(count==api_num){
+			context.succeed(event);
+    	}
+    });
+}
+
+
+
 /*
 python cx.py /profile/user '{"id":"64f3fb36-7a9b-4156-a584-bd9a86d0ddfa", "type":"dac"}'
 
@@ -116,20 +148,30 @@ function retrieve_profile_user(event, context, userid){
 		res.on('data', function(chunk){
 			console.log("----data chunk of retrieve_profile_user---");
 			console.log(chunk);
+			var profile_user_obj = JSON.parse(chunk);
+			
+			if(user_primary_key){
+				set_userdata(event, context, user_primary_key,"profile_user", profile_user_obj )
+			}else{
+				console.log("--------primary key cannot be found--------");
+				count++;
+				if(count==api_num){
+					context.succeed(event);
+				}
+			}
 		});
 		res.on('end', function(){
 			console.log("request ended successfully in retrieve_profile_user");
-    		context.succeed(event);
-			count++;
-			if(count==api_num){
-				context.succeed(event);
-	    	}
+//    		context.succeed(event);
 		});
 	});
 
 	req.on("error", function(error){
 		console.log("request fail fail fail in retrieve_profile_user");
-		context.succeed(event);
+		count++;
+		if(count==api_num){
+			context.succeed(event);
+	    }
 	});
 
 	req.write(postData);
@@ -167,12 +209,23 @@ function retrieve_traffic_keyword(event, context, userid){
 		res.on('data', function(chunk){
 			console.log("----data chunk of retrieve_traffic_keyword----");
 			console.log(chunk);
+			var traffic_keywor_obj = JSON.parse(chunk);
+
+			if(user_primary_key){
+				set_userdata(event, context, user_primary_key,"traffic_keyword", traffic_keywor_obj )
+			}else{
+				console.log("-------------primary key cannot be found-----------");
+				count++;
+				if(count==api_num){
+					context.succeed(event);
+				}
+			}
+
+
 		});
 		res.on('end', function(){
 			console.log("request ended successfully in retrieve_traffic_keyword");
-			if(count==api_num){
-    			context.succeed(event);
-    		}
+//			count++;
 		});
 	});
 
@@ -220,12 +273,23 @@ function retrieve_traffic_event(event, context, userid){
 		res.on('data', function(chunk){
 			console.log("----data chunk of retrieve_traffic_event----");
 			console.log(chunk);
+			var retrieve_traffic_obj = JSON.parse(chunk);
+
+			if(user_primary_key){
+				set_userdata(event, context, user_primary_key,"traffic_event", retrieve_traffic_obj )
+			}else{
+				console.log("----------primary key cannot be found----------");
+				count++;
+				if(count==api_num){
+					context.succeed(event);
+				}
+			}
+
 		});
 		res.on('end', function(){
 			console.log("request ended successfully in retrieve_traffic_event");
-			if(count==api_num){
-    			context.succeed(event);
-    		}
+//			count++;
+
 		});
 	});
 
@@ -239,7 +303,5 @@ function retrieve_traffic_event(event, context, userid){
 
 	req.write(postData);
 	req.end();
-
-
 
 }
